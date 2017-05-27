ed4d9a9fc08942dc084eddba86d194b4afd63a40
MeisterTR <rensen26@gmail.com>
2017-05-27 07:26:25 +0200

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -3,8 +3,8 @@ systemDictionary = {
         "de": "MiHome-vacuum Adapter Einstellungen",
         "ru": "Настройки драйвера MiHome пылесоса"
     },
-    "IP address: ":     {"en": "IP address: ",      "de": "IP-Adresse des Roboters: ",  "ru": "IP адрес пылесоса: "},
-    "Lamp port: ":     {"en": "Lamp port: ",     "de": "Port des Roboters: ",        "ru": "Порт пылесоса: "},
+    "IP address: ":     {"en": "IP address: ",      "de": "IP-Adresse der Lampe: ",     "ru": "IP адрес: "},
+    "Lamp port: ":     {"en": "Lamp port: ",        "de": "Port der Lampe: ",           "ru": "Порт пылесоса: "},
     "Own port: ":       {"en": "Own port: ",        "de": "Eigener Port: ",             "ru": "Собственный порт: "},
     "Token: ":          {"en": "Token: ",           "de": "Token: ",                    "ru": "Токен: "}
 };
