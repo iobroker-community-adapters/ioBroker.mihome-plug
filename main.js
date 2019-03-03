9ad9c4edb4cf8b27e740c6d7e8828f72a4bdf44f
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:44:22 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 'use strict';
 
 // you have to require the utils module and call adapter function
-const utils   = require(__dirname + '/lib/utils'); // Get common adapter utils
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 const adapter = new utils.Adapter('mihome-plug');
 const dgram   = require('dgram');
 const MiHome  = require('./lib/mihomepacket');
